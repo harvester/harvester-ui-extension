86696b831af46d744dd6b6ae7f21923160964731
Andy Lee <andy.lee@suse.com>
2025-05-06 11:26:53 +0800

    Add 1.4.2/1.5.1/1.6.0 feature flags
    Signed-off-by: Andy Lee <andy.lee@suse.com>

--- pkg/harvester/config/feature-flags.js
+++ pkg/harvester/config/feature-flags.js
@@ -40,14 +40,19 @@ const featuresV141 = [
   ...featuresV140
 ];
 
-// TODO: add v1.4.2 official release note
+// https://github.com/harvester/dashboard/releases/tag/v1.4.2
 const featuresV142 = [
   ...featuresV141,
   'refreshIntervalInSecond',
   'allowEmptySnapshotClassName'
 ];
 
-// TODO: add v1.5.0 official release note
+// TODO: https://github.com/harvester/dashboard/releases/tag/v1.4.3
+const featuresV143 = [
+  ...featuresV142,
+];
+
+// https://github.com/harvester/harvester-ui-extension/releases/tag/v1.5.0
 const featuresV150 = [
   ...featuresV142,
   'tpmPersistentState',
@@ -57,6 +62,18 @@ const featuresV150 = [
   'thirdPartyStorage'
 ];
 
+// TODO: https://github.com/harvester/harvester-ui-extension/releases/tag/v1.5.1
+const featuresV151 = [
+  ...featuresV150
+];
+
+
+// TODO: https://github.com/harvester/harvester-ui-extension/releases/tag/v1.6.0
+const featuresV160 = [
+  ...featuresV151
+];
+
+
 export const RELEASE_FEATURES = {
   'v1.3.0': featuresV130,
   'v1.3.1': featuresV131,
@@ -65,5 +82,8 @@ export const RELEASE_FEATURES = {
   'v1.4.0': featuresV140,
   'v1.4.1': featuresV141,
   'v1.4.2': featuresV142,
-  'v1.5.0': featuresV150
+  'v1.4.3': featuresV143,
+  'v1.5.0': featuresV150,
+  'v1.5.1': featuresV151,
+  'v1.6.0': featuresV160
 };
