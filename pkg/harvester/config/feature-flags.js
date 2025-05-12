7a22b39984323e6f57c518dc8822cd850dc7acc8
Andy Lee <andy.lee@suse.com>
2025-05-12 10:43:12 +0800

    Refactor feature flags structure (#276)
    * refactor feature flags structure
    
    Signed-off-by: Andy Lee <andy.lee@suse.com>
    
    * Replace with FEATURE_FLAGS and valid and sort versions
    
    Signed-off-by: Andy Lee <andy.lee@suse.com>
    
    ---------
    
    Signed-off-by: Andy Lee <andy.lee@suse.com>
    (cherry picked from commit f8079c592485c51155f59037b395c611c04d327a)
    
    # Conflicts:
    #	pkg/harvester/config/feature-flags.js

--- pkg/harvester/config/feature-flags.js
+++ pkg/harvester/config/feature-flags.js
@@ -1,8 +1,6 @@
-// https://github.com/harvester/dashboard/releases/tag/v1.3.0
-const featuresV130 = [
-  'supportHarvesterClusterVersion'
-];
+import semver from 'semver';
 
+<<<<<<< HEAD
 // https://github.com/harvester/dashboard/releases/tag/v1.3.1
 const featuresV131 = [
   ...featuresV130,
@@ -66,4 +64,59 @@ export const RELEASE_FEATURES = {
   'v1.4.1': featuresV141,
   'v1.4.2': featuresV142,
   'v1.5.0': featuresV150
+=======
+const FEATURE_FLAGS = {
+  'v1.3.0': [
+    'supportHarvesterClusterVersion'
+  ],
+  'v1.3.1': [
+    'autoRotateRke2CertsSetting',
+    'supportBundleNodeCollectionTimeoutSetting'
+  ],
+  'v1.3.2': [
+    'kubeconfigDefaultTokenTTLMinutesSetting',
+    'improveMaintenanceMode',
+  ],
+  'v1.3.3': [],
+  'v1.4.0': [
+    'cpuPinning',
+    'usbPassthrough',
+    'volumeEncryption',
+    'schedulingVMBackup',
+    'vmSnapshotQuota',
+    'longhornV2LVMSupport',
+    'improveMaintenanceMode',
+  ],
+  'v1.4.1': [],
+  'v1.4.2': [
+    'refreshIntervalInSecond',
+    'allowEmptySnapshotClassName'
+  ],
+  'v1.4.3': [],
+  'v1.5.0': [
+    'tpmPersistentState',
+    'efiPersistentState',
+    'untaggedNetworkSetting',
+    'skipSingleReplicaDetachedVol',
+    'thirdPartyStorage'
+  ],
+  'v1.5.1': [],
+  'v1.6.0': []
+>>>>>>> f8079c5 (Refactor feature flags structure (#276))
 };
+
+const generateFeatureFlags = () => {
+  const versions = [...Object.keys(FEATURE_FLAGS)].filter((version) => semver.valid(version)).sort(semver.compare);
+
+  const generatedFlags = {};
+
+  versions.forEach((version, index) => {
+    const previousVersion = versions[index - 1];
+
+    generatedFlags[version] = previousVersion ? [...generatedFlags[previousVersion], ...FEATURE_FLAGS[version]] : [...FEATURE_FLAGS[version]];
+  });
+
+  return generatedFlags;
+};
+
+export const RELEASE_FEATURES = generateFeatureFlags();
