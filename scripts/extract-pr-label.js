d6caca6414486a757abd7a287d879354538b3c0d
Andy Lee <andy.lee@suse.com>
2025-08-18 11:04:29 +0800

    ci: add auto PR label workflow and script
    Signed-off-by: Andy Lee <andy.lee@suse.com>

---
+++ scripts/extract-pr-label.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+if (process.argv.length < 3) {
+  console.error('No PR title as first argument');
+  process.exit(1);
+}
+
+const LABEL_MAP = {
+  bug:         'bugfix',
+  feat:        'feature',
+  docs:        'documentation',
+  enhancement: 'enhancement',
+  refactor:    'refactor',
+  style:       'style',
+  chore:       'chore',
+  ci:          'ci',
+  other:       'other'
+};
+const prTitle = process.argv[2];
+const prLabel = prTitle.split(':')[0].trim();
+
+console.log(LABEL_MAP[prLabel] || 'other');
