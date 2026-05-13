5d8d7ec9864ee915d400ba8296b1fb842ee29c12
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-05-13 13:46:03 +0200

    feat(forklift): fixed to fix the icon on extension
    Signed-off-by: Marcelo Fukumoto <marcelo.fukumoto@suse.com>

--- pkg/harvester/vue.config.js
+++ pkg/harvester/vue.config.js
@@ -1,1 +1,18 @@
-module.exports = require('@rancher/shell/pkg/vue.config')(__dirname);
+const path = require('path');
+
+const config = require('@rancher/shell/pkg/vue.config')(__dirname);
+const originalConfigureWebpack = config.configureWebpack;
+
+config.configureWebpack = function(cfg) {
+  if (originalConfigureWebpack) {
+    originalConfigureWebpack(cfg);
+  }
+
+  // Force @rancher/icons to resolve to the top-level 2.0.60 version
+  // so new icons (datastore, disk, network, etc.) are available in extension mode.
+  const maindir = path.resolve(__dirname, '..', '..');
+
+  cfg.resolve.alias['@rancher/icons'] = path.resolve(maindir, 'node_modules', '@rancher/icons');
+};
+
+module.exports = config;
