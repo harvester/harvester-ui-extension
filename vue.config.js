d67813eb39efc427afe3e7c717d0d9cdae34c03e
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-05-27 14:35:47 +0200

    feat(forklift): Removed wrong configs used for testing
    Signed-off-by: Marcelo Fukumoto <marcelo.fukumoto@suse.com>

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,6 @@
-const path = require('path');
-const baseConfig = require('@rancher/shell/vue.config');
+const config = require('@rancher/shell/vue.config');
 
-const shellConfig = baseConfig(__dirname, {
+module.exports = config(__dirname, {
   excludes: [],
   // excludes: ['harvester']
 });
-
-const originalConfigureWebpack = shellConfig.configureWebpack;
-
-shellConfig.configureWebpack = function(config) {
-  if (originalConfigureWebpack) {
-    originalConfigureWebpack(config);
-  }
-
-  // Force all @rancher/icons imports (including from @rancher/shell) to use
-  // the top-level 2.0.60 version so new icons are available everywhere.
-  config.resolve.alias['@rancher/icons'] = path.resolve(__dirname, 'node_modules', '@rancher/icons');
-};
-
-module.exports = shellConfig;
