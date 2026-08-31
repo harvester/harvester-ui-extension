55bb650b0adfa6a3f07e62b02e4d3a4c4766a388
Andy Lee <andy.lee@suse.com>
2026-08-31 16:19:39 +0800

    feat: add rancher-k3k addon UI
    Signed-off-by: Andy Lee <andy.lee@suse.com>

--- pkg/harvester/models/harvesterhci.io.addon.js
+++ pkg/harvester/models/harvesterhci.io.addon.js
@@ -5,12 +5,14 @@ import HarvesterResource from './harvester';
 import { HCI } from '../types';
 
 const HARVESTER_NVIDIA_DRIVER_TOOLKIT = 'harvester-system/nvidia-driver-toolkit';
+const RANCHER_VCLUSTER = 'harvester-system/rancher-vcluster';
+const RANCHER_K3K = 'k3k-system/k3k';
 
 export default class HciAddonConfig extends HarvesterResource {
   get availableActions() {
     const out = super._availableActions;
 
-    if (this.id === 'harvester-system/rancher-vcluster') {
+    if (this.id === RANCHER_VCLUSTER || this.id === RANCHER_K3K) {
       const rancherDashboard = {
         action:  'goToRancher',
         enabled: this.spec.enabled,
@@ -69,18 +71,19 @@ export default class HciAddonConfig extends HarvesterResource {
   }
 
   goToRancher() {
-    const valuesContent = jsyaml.load(this.spec.valuesContent);
-
     window.open(
-      `https://${ valuesContent.hostname }`,
+      this.rancherHostname,
       '_blank',
     );
   }
 
   get rancherHostname() {
-    const valuesContent = jsyaml.load(this.spec.valuesContent);
+    const valuesContent = jsyaml.load(this.spec.valuesContent) || {};
 
-    return `https://${ valuesContent.hostname }`;
+    // rancher-k3k nests the hostname under `rancher`, rancher-vcluster keeps it at the top level
+    const hostname = this.id === RANCHER_K3K ? valuesContent.rancher?.hostname : valuesContent.hostname;
+
+    return `https://${ hostname }`;
   }
 
   get stateColor() {
